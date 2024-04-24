47216702428dbf85011f243531ee8868d0f62ea9
VictoriaSCorreia <104169748+VictoriaSCorreia@users.noreply.github.com>
2024-04-24 18:04:57 -0300

    updating

--- Site/index.js
+++ Site/index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("express")();
 
-app.get("/", (req,res)=> res.sendFile(__dirname + "/create.html"))
+app.get("/", (req,res)=> res.sendFile(__dirname + "/path.html"))
 app.listen(8081, ()=>console.log("Listening on http port 8081"))
 const websocketServer = require("websocket").server
 const httpServer = http.createServer();
